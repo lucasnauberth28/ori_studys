a765df22f12688f2947f96158328d65a0a38d4a8
Nauberth <lucasnauberth@gmail.com>
2023-05-16 18:56:43 -0300

    avanço

---
+++ js-completo/aulas/04-05-number&math/script.js
@@ -0,0 +1,16 @@
+// Retorne um número aleatório
+// entre 1050 e 2000
+const maxNumber = Math.floor(Math.random() * (2000 - 1050 + 1)) + 1050;
+
+// Retorne o maior número da lista abaixo
+const numeros = '4, 5, 20, 8, 9';
+const spliceNumbers = numeros.split(', ');
+const arrayNumbers = Math.max(...spliceNumbers);
+console.log(arrayNumbers);
+
+
+// Crie uma função para limpar os preços
+// e retornar os números com centavos arredondados
+// depois retorne a soma total
+const listaPrecos = ['R$ 59,99', ' R$ 100,222',
+                     'R$ 230  ', 'r$  200'];
